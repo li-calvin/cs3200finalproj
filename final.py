c64950c35506a24e995204fb12dbfb8054de026d
li-calvin <licalvin5858@gmail.com>
2023-06-18 00:03:42 -0400

    creating the initial functionalities of the user terminal interaction

--- final.py
+++ final.py
@@ -1,7 +1,3 @@
-test
-
-test again
-
 import pymysql
 import matplotlib.pyplot as plt
 
@@ -31,6 +27,10 @@ def work(name):
 
     # Check user's choice
     if choice.lower() == "yes":
+        # Initialize variables to store user input
+        ausername = ""
+        apassword = ""
+
         # Loop until valid credentials are provided
         while ausername != admin_username or apassword != admin_password:
             # Prompt user for username and password
@@ -47,26 +47,130 @@ def work(name):
                 print("Access denied. Invalid username or password. Please try again.")
     else:
         # User doesn't want to perform administrative tasks
-        pass
+        print("Welcome to the main menu")
+
+        # Prompt user for additional options
+        while True:
+            print("\nPlease select an option:")
+            print("1. Create new medical device entry")
+            print("2. View your information")
+            print("3. View devices")
+            print("0. Exit")
+            option = input("Enter your choice (0-3): ")
+
+            if option == "1":
+                # Option 1: Create new medical device entry
+                print("Creating new medical device entry...")
+                # Add your code for creating a new medical device entry here
+
+            elif option == "2":
+                while True:
+                    # Option 2: View user information
+                    print("Viewing your devices used ...")
+
+                    try:
+                        print("Filter by Doctor First Name, Last Name ")
+                        first = input("Enter doctor first name: ")
+                        last = input("Enter doctor last name: ")
+
+                        c3 = connection.cursor()  # shouldn't reuse cursors
+                        # calling procedure by its string
+                        c3.callproc('filterDoctor', [first, last])  # second part department, is the list of parameters to the specific proceudre get_department
+                        for row in c3.fetchall():
+                            print(row)
+                        c3.close()
+                    except pymysql.Error as e:
+                        code, msg = e.args
+                        print("Error retrieving data from the db", code, msg)
+
+                    # Ask the user if they want to return to the main menu
+                    return_to_menu = input("Do you want to return to the main menu? (yes/no): ")
+                    if return_to_menu.lower() == "yes":
+                        break
+                    else:
+                        continue
+
+            elif option == "3":
+                while True:
+                    # Option 3: View devices
+                    print("Viewing devices...")
+                    # Add your code for viewing devices here
+                    print("ordering the device by quantity")
+                    try:
+                        print("Order Device ")
+                        c5 = connection.cursor()  # shouldn't reuse cursors
+                        # calling procedure by its string
+                        c5.callproc(
+                            'mostQuantity2', )  # second part department, is the list of parameters to the specific proceudre get_department
+                        all_device = c5.fetchall()
+                        for row in all_device:
+                            print(row)
+
+                        quantities = [row['quantity'] for row in all_device]
+                        devices = [row['device_id'] for row in all_device]
+                        plt.bar(devices, quantities)
+                        plt.xlabel('Device')
+                        plt.ylabel('Quantity')
+                        plt.title('Device Quantity')
+                        plt.show()
+                        c5.close()
+
+
+                    except pymysql.Error as e:
+                        code, msg = e.args
+                        print("Error retrieving data from the db", code, msg)
+
+                    while True:
+                        print("\nPlease select a filtering option:")
+                        print("1. Filter devices by category")
+                        print("2. Filter devices by price range")
+                        print("3. Filter devices by availability")
+                        print("0. Return to the main menu")
+                        filter_option = input("Enter your choice (0-3): ")
+
+                        if filter_option == "1":
+                            # Add code to filter devices by category
+                            try:
+                                print("Filter by Device Type")
+                                type = input("Enter Device Type: ")
+                                c1 = connection.cursor()  # shouldn't reuse cursors
+                                # calling procedure by its string
+                                c1.callproc('filterType', (
+                                type,))  # second part department, is the list of parameters to the specific proceudre get_department
+                                for row in c1.fetchall():
+                                    print(row)
+
+                            except pymysql.Error as e:
+                                code, msg = e.args
+                                print("Error retrieving data from the db", code, msg)
+
+                        elif filter_option == "0":
+                            # Return to the main menu
+                            break
+
+                        else:
+                            # Invalid option
+                            print("Invalid option. Please choose again.")
+
+                    # Ask the user if they want to return to the main menu
+                    return_to_menu = input("Do you want to return to the main menu? (yes/no): ")
+                    if return_to_menu.lower() == "yes":
+                        break
+                    else:
+                        continue
+
+            elif option == "0":
+                # Option 0: Exit the program
+                print("Exiting the program. Goodbye!")
+                break
+
+            else:
+                # Invalid option
+                print("Invalid option. Please choose again.")
 
 
 
     """
-    print("Several Filter Functionalities")
-    try:
-        print("Filter by Device Type")
-        type = input("Enter Device Type: ")
-        c1 = connection.cursor()  # shouldn't reuse cursors
-        # calling procedure by its string
-        c1.callproc('filterType', (type,))  # second part department, is the list of parameters to the specific proceudre get_department
-        for row in c1.fetchall():
-            print(row)
-
-    except pymysql.Error as e:
-        code, msg = e.args
-        print("Error retrieving data from the db", code, msg)
-
-
     try:
         print("Filter by Room")
         room = input("Enter Room: ")
@@ -81,20 +185,6 @@ def work(name):
         code, msg = e.args
         print("Error retrieving data from the db", code, msg)
 
-    try:
-        print("Filter by Doctor First Name, Last Name ")
-        first = input("Enter doctor first name: ")
-        last = input("Enter doctor last name: ")
-
-        c3 = connection.cursor() #shouldn't reuse cursors
-        # calling procedure by its string
-        c3.callproc('filterDoctor', [first, last]) #second part department, is the list of parameters to the specific proceudre get_department
-        for row in c3.fetchall():
-            print(row)
-        c3.close()
-    except pymysql.Error as e:
-        code, msg = e.args
-        print("Error retrieving data from the db", code, msg)
     
 
     print("There are other functionailities that exist such as moving the medial device to the appropriate room that the device is needed in ")
@@ -114,28 +204,7 @@ def work(name):
 
 
 
-    print("ordering the device by quantity")
-    try:
-        print("Order Device ")
-        c5 = connection.cursor()  # shouldn't reuse cursors
-        # calling procedure by its string
-        c5.callproc('mostQuantity2', )  # second part department, is the list of parameters to the specific proceudre get_department
-        all_device = c5.fetchall()
-        for row in all_device:
-            print(row)
-
-        quantities = [row['quantity'] for row in all_device]
-        devices = [row['device_id'] for row in all_device]
-        plt.bar(devices, quantities)
-        plt.xlabel('Device')
-        plt.ylabel('Quantity')
-        plt.title('Device Quantity')
-        plt.show()
-        c5.close()
-
-    except pymysql.Error as e:
-        code, msg = e.args
-        print("Error retrieving data from the db", code, msg)
+    
     
     print("List of all Examinations of a specific patient")
     try:
